4f92604715ac4072d579daf20992b07d5a9d9a62
mzilliox <matthieu.zilliox@novencia.com>
2016-03-31 12:41:02 +0200

    Add first version

---
+++ MessageCopyForSentAsEnabled/MessageCopyForSendAsEnabled.ps1
@@ -0,0 +1,55 @@
+﻿<#
+
+    .SYNOPSYS
+        Script modifiant le comportement d'une boite aux lettres partagée
+
+    .DESCRIPTION
+        Ce script se connecte à Office 365 et modifie la propriété "MessageCopyForSentAsEnabled".
+        Tout email envoyé depuis une boîte aux lettres partagée sera placé dans le dossier "Elements envoyés" de la boîte partagée en question.
+
+    .PARAMETER
+		Un tableau comportant les noms des boîtes emails à modifier
+
+	.EXAMPLE
+		.\MessageCopyForSendAsEnabled.ps1 -mailBox sharedBox1, sharedBox2, sharedBox3 [...]
+ 
+#>
+
+
+Param(
+[parameter(Mandatory=$true)][string[]]$mailBox)
+
+function Set-MessageCopyForSentAsEnabled($mailBox)
+{
+
+    # Connexion à Office 365
+    $LiveCred = Get-Credential
+
+    # Connexion à Exchange Online et Import de PSSession
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+    Import-PSSession $Session
+
+
+    foreach ($i in $mailBox)
+    {
+        # Modification de la boîte au lettre partagée saisie par l'utilisateur
+        Set-mailbox $i -MessageCopyForSentAsEnabled $true
+
+        Write-Host "Mise à jour de '$i' effectué avec succès" -ForegroundColor DarkGreen
+        Write-Host
+    }
+
+    Write-Host
+    Write-Host "L'ensemble des modifications ont été opérées avec succès !" -ForegroundColor Green
+    Write-Host
+
+    Remove-PSSession $Session
+	
+	Write-Host
+	Write-Host "Déconnexion d'Office 365." -ForegroundColor Red
+	Write-Host
+	Write-Host "Fin du script." -ForegroundColor Red
+}
+
+
+Set-MessageCopyForSentAsEnabled($mailBox)
---
+++ SetImmutableIDToExtentionAttribute/SetImmutableIDToExtentionAttribute.ps1
@@ -0,0 +1,79 @@
+﻿<#
+
+	.SYNOPSYS
+		Ce script a pour but de générer un ImmutableID pour des objets de l'AD 
+		afin de contrôler la synchronisation avec AZURE
+
+	.DESCIPTION
+		Il est possible de générer un ImmutableID pour des utilisateurs ou pour des groupes.
+		Une fois la cible choisi, le script va utiliser l'attribut OBJECTGUID pour le transformer en Base64
+		et l'ingecter dans l'attribut ExtentionAttribute1.
+		ATTENTION : Il est nécessaire d'indiquer à Azure Connect AD quel attribut il doit prendre en compte pour la synchronisation de l'annuaire.
+
+	.EXEMPLE
+		.\SetImmutableIDToExtentionAttribute "OU=monOU,DC=domain,DC=TLD" "Group" "ExtentionAttribute1"
+
+		Premier argument : L'OU à scanner
+		Deuxième argument : Le type d'object à scanner et à modifier
+		Troisième argument : L'attribut cible pour l'ImmutableID
+
+	.REMERCIEMENTS
+		Ce script utilise lui même le script "GUID2ImmutableID" de Steve HALLIGAN https://gallery.technet.microsoft.com/office/Covert-DirSyncMS-Online-5f3563b1
+
+	.NOTES
+		Auteur : Matthieu ZILLIOX
+		Créé le : 25 mars 2016
+		Twitter : @matthieuzilliox
+
+#>
+
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$OU,
+	[string]$ObjectType,
+	[string]$adAttribute
+)
+
+# LIST OF used variables
+# $Groups
+# Group
+#
+
+
+#Setting up variables
+Write-Host
+Write-Host
+Write-Host "Setting up initial variables... " -NoNewline
+
+$Groups = GetADGroup -searchbase -Filter "*"
+
+Write-Host
+Write-Host
+Write-Host "Start the procedure... " -ForegroundColor DarkGray
+
+
+
+foreach ($Group in $Groups) {
+
+    Write-Host
+    Write-Host " Start with Group : $($Group.Name)"
+
+
+}
+
+
+function convertTheValue ($valuetoconvert) {
+	
+	try
+	{
+		$guid = [GUID]$valuetoconvert
+	    $bytearray = $guid.tobytearray()
+		$immutableID = [system.convert]::ToBase64String($bytearray)
+    
+		return  $immutableID
+	}
+	catch
+	{
+		return 0
+	}
+}
