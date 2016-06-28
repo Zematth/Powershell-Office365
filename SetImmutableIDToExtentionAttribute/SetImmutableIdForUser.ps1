2f1d36f6209e53fc83e774ffe0c99f497bec5051
mzilliox <matthieu.zilliox@novencia.com>
2016-06-28 09:01:37 +0200

    Ajout fichier SetImmutableIdForUser.ps1

---
+++ SetImmutableIDToExtentionAttribute/SetImmutableIdForUser.ps1
@@ -0,0 +1,67 @@
+<#
+
+	.SYNOPSYS
+		Ce script a pour but de générer un ImmutableID pour un object spécifique 
+		afin de contrôler la synchronisation avec AZURE
+
+	.DESCIPTION
+		Ce script génère un ImmutableId pour un utilisateur spécifique fournis lors de l'exécution du script.
+		Le script récupère le l'ObjectGUID de l'utilisateur, le transforme en Base64 et enfin enregistre cet ImmutableId
+		dans l'attribut 'ExtentionAttribute1' ou tout autre attribut spécifié.
+
+		ATTENTION : Il est nécessaire d'indiquer à Azure Connect AD quel attribut il doit prendre en compte pour la synchronisation de l'annuaire.
+
+	.EXEMPLE
+		.\SetImmutableIDToExtentionAttribute "samAccountName"
+
+		Premier argument : Obligatoire. Le SamAccountName de l'utilisateur cible.
+		
+	.NOTES
+		Auteur : Matthieu ZILLIOX
+		Créé le : 27 juin 2016
+		Twitter : @matthieuzilliox
+
+#>
+
+Param (
+    [Parameter(Mandatory=$true)]
+    [String]$sama    )
+
+Import-Module ActiveDirectory
+
+Write-Host "Initialisation des variables..."
+$Guid = ""
+$iid = ""
+$immutableID = ""
+
+function ConvertObjectGuidToBase64 ($data)
+{
+    $Guid = $data
+    $bytearray = $guid.tobytearray()
+    $immutableID = [system.convert]::ToBase64String($bytearray)
+}
+
+try
+{
+	$var = Get-ADUser -Identity $sama
+
+	$bytearray = $var.ObjectGUID.tobytearray()
+	$immutableID = [system.convert]::ToBase64String($bytearray)
+
+	Write-Host
+	Write-Host "L'ObjectGUID de" $sama "est :" $var.ObjectGUID.ToString()
+	Write-Host "L'immutable ID de" $sama "est :" $immutableID -ForegroundColor Cyan
+	Write-Host 
+	Write-Host "Enregistrement de l'immutableID... "
+
+	Set-AdUser -Identity $sama -Add @{extensionAttribute1 = $immutableID}
+
+	Write-host "Enregistrement effectué avec succès." -ForegroundColor Green
+	Write-Host "Fin du script..." -ForegroundColor Green
+}
+catch
+{
+	Write-Host "Une erreur s'est produite. La voici :"
+	Write-Host $_.Exception.Message -ForegroundColor Red
+}
+
